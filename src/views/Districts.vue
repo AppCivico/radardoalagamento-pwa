ef389677eb402c30b2386df360be8c0483209b72
Laís Tomaz <lais@eokoe.com>
2018-07-10 12:53:04 -0300

    Create Districts view

---
+++ src/views/Districts.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <Header />
+    <h1>Districts</h1>
+    <Footer />
+  </main>
+</template>
+
+<script>
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+
+export default {
+  name: 'Districts',
+  components: {
+    Header,
+    Footer,
+  }
+}
+</script>
+
