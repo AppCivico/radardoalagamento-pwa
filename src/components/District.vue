0d37bc16cefb69d7e81239204586aa8665cdcea9
Laís Tomaz <lais@eokoe.com>
2018-07-10 17:20:57 -0300

    Enhance District style

--- src/components/District.vue
+++ src/components/District.vue
@@ -1,7 +1,10 @@
 <template>
   <li>
-    <input type="checkbox" v-model="checkbox" name="checkbox" @change="handleCheckbox">
-    {{ district.name }}
+    <div class="district__checkbox">
+      <input type="checkbox" v-model="checkbox" :id="`district__${district.id}`" name="district__checkbox" @change="handleCheckbox">
+      <span></span>
+    </div>
+    <label :for="`district__${district.id}`">{{ district.name }}</label>
   </li>
 </template>
 
@@ -27,3 +30,48 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.districts li {
+  float: left;
+  width: 50%;
+  text-align: left;
+  margin-bottom: $gutter / 2;
+}
+.districts .district__checkbox {
+  position: relative;
+  width: 20px;
+  height: 20px;
+  display: inline-block;
+  vertical-align: middle;
+  margin-right: $gutter / 3;
+}
+.districts .district__checkbox span {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url('../assets/images/checkbox.png');
+  background-size: 100% auto;
+  z-index: 1;
+}
+.districts .district__checkbox input[type=checkbox] {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 2;
+  opacity: 0;
+  margin-right: $gutter / 3;
+
+  &:checked + span {
+    background-image: url('../assets/images/checkbox-on.png');
+  }
+}
+.districts label {
+  font-size: 1.4em;
+}
+</style>
+
