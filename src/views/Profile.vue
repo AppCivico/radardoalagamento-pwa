03d77911cd0096288ff76b3db0ea96a1e1102ed7
Laís Tomaz <lais@eokoe.com>
2018-07-10 17:53:46 -0300

    Create Profile view

---
+++ src/views/Profile.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+    <Header :check="true"/>
+      <section id="profile">
+        <h2>Profile</h2>
+      </section>
+    <Footer />
+  </main>
+</template>
+
+<script>
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+
+export default {
+  name: 'home',
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
+
+<style>
+</style>
+
