85b66c78b5b164ef9d07018b9b4b41025b3d2e57
Laís Tomaz <lais@eokoe.com>
2018-07-10 17:56:07 -0300

    Add SET_SELECTED_DISTRICTS mutation to store

--- src/store.js
+++ src/store.js
@@ -8,14 +8,19 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     zones: [],
+    selectedDistricts: [],
   },
   mutations: {
     SET_ZONES(state, { res }) {
       state.zones = res.results;
     },
+    SET_SELECTED_DISTRICTS(state, { payload }) {
+      console.log('pay', payload);
+      state.selectedDistricts = payload;
+    },
   },
   actions: {
-    GET_ZONES({commit}) {
+    GET_ZONES({ commit }) {
       return new Promise((resolve, reject) => {
         axios.get(`${config.api}/zone`).then(
           (response) => {
@@ -29,5 +34,5 @@ export default new Vuex.Store({
         );
       });
     },
-  }
+  },
 });
