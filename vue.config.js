7359e02588679c048e3b73b8578e7e0ecb256906
Laís Tomaz <lais@eokoe.com>
2018-07-10 10:40:34 -0300

    Add vue config gile

---
+++ vue.config.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+module.exports = {
+  lintOnSave: false,
+  css: {
+    loaderOptions: {
+      sass: {
+        data: fs.readFileSync('src/styles/_variables.scss', 'utf-8'),
+      },
+    },
+  },
+  pwa: {
+    name: 'Radar do Alagamento',
+    themeColor: '#28cfce',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'no',
+    workboxOptions: {
+      include: [/\.html$/, /\.js$/, /\.css$/, /\.svg$/, /\.png$/, /\.jpg$/],
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('^https://dtupa.eokoe.com/'),
+          handler: 'networkFirst',
+        },
+        {
+          urlPattern: /\.svg$/,
+          handler: 'networkFirst',
+        },
+      ],
+    },
+  },
+};
