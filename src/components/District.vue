ebaedbbb0d624a9c78b5abdd065a3c1bfaf6ad69
Laís Tomaz <lais@eokoe.com>
2018-07-10 16:36:14 -0300

    Create District component

---
+++ src/components/District.vue
@@ -0,0 +1,29 @@
+<template>
+  <li>
+    <input type="checkbox" v-model="checkbox" name="checkbox" @change="handleCheckbox">
+    {{ district.name }}
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'District',
+  props: {
+    district: Object,
+  },
+  data() {
+    return {
+      checkbox: false,
+    };
+  },
+  methods: {
+    handleCheckbox() {
+      if (this.checkbox) {
+        this.$emit('add-district', this.district.id);
+      } else {
+        this.$emit('remove-district', this.district.id);
+      }
+    },
+  }
+}
+</script>
