803bbfdc2e74bfa8d8d4f0f7f5000c3d12254beb
Laís Tomaz <lais@eokoe.com>
2018-07-10 16:35:28 -0300

    Add Zones related properties to store

--- src/store.js
+++ src/store.js
@@ -1,16 +1,33 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
+import config from '@/config';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    zones: [],
   },
   mutations: {
-
+    SET_ZONES(state, { res }) {
+      state.zones = res.results;
+    },
   },
   actions: {
-
-  },
+    GET_ZONES({commit}) {
+      return new Promise((resolve, reject) => {
+        axios.get(`${config.api}/zone`).then(
+          (response) => {
+            commit('SET_ZONES', { res: response.data });
+            resolve();
+          },
+          (err) => {
+            reject(err.response);
+            console.error(err);
+          },
+        );
+      });
+    },
+  }
 });
