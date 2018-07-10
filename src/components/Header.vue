e2997c97dc4c98fa318818b1430a55e47cc69072
Laís Tomaz <lais@eokoe.com>
2018-07-10 12:51:26 -0300

    Create Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header id="header">
+    <h2>{{ this.$route.name }}</h2>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style lang="scss">
+#header {
+  padding: $gutter * 1.5 $gutter;
+  background-color: $color_blue;
+  text-align: left;
+}
+#header h2 {
+  font-size: 2.2em;
+  font-weight: 400;
+  color: $color_white;
+}
+</style>
+
+
