eb8afc97f5af37ccf7196c797bee0f9c728cbf6e
Laís Tomaz <lais@eokoe.com>
2018-07-11 15:50:35 -0300

    Create Options view component

---
+++ src/views/Options.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <Header />
+    <section id="options">
+        <h1>Configurações</h1>
+    </section>
+    <Footer />
+  </main>
+</template>
+
+<script>
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import swal from 'sweetalert';
+
+export default {
+  name: 'Options',
+  components: {
+    Header,
+    Footer,
+  },
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
