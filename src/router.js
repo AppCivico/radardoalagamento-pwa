3ebf3c5ab8e071007c849ba18453fc2378bfc66d
Laís Tomaz <lais@eokoe.com>
2018-07-11 18:05:11 -0300

    Change Configurações router name to Ajuda

--- src/router.js
+++ src/router.js
@@ -36,7 +36,7 @@ export default new Router({
     },
     {
       path: '/options',
-      name: 'Configurações',
+      name: 'Ajuda',
       component: () => import('./views/Options.vue'),
     },
     {
