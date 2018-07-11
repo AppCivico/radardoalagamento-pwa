692c7bec2e5539b53a154dfc14b1bc4084455c20
Laís Tomaz <lais@eokoe.com>
2018-07-11 10:20:46 -0300

    Enhance check style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header id="header">
     <h2>{{ this.$route.name }}</h2>
-    <a v-if="check" href="#" @click.prevent="$emit('check')">check</a>
+    <a v-if="check" href="#" class="header__check" @click.prevent="$emit('check')">check</a>
   </header>
 </template>
 
@@ -16,6 +16,7 @@ export default {
 
 <style lang="scss">
 #header {
+  overflow: hidden;
   padding: $gutter * 1.5 $gutter;
   background-color: $color_blue;
   text-align: left;
@@ -24,6 +25,18 @@ export default {
   font-size: 2.2em;
   font-weight: 400;
   color: $color_white;
+  float: left;
+}
+.header__check {
+  display: block;
+  float: right;
+  font-size: 0;
+  background-image: url('../assets/images/done.png');
+  background-repeat: no-repeat;
+  background-size: 100% auto;
+  width: 30px;
+  height: 23px;
+  margin-top: 3px;
 }
 </style>
 
